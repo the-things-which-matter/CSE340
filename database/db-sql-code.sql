c4d2deb081a1423bf14e3b831c0ae6e1d5f9797a
the-things-which-matter <igabrigham9@gmail.com>
2025-06-14 15:28:58 +0300

    added inventory update query to database folder

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -257,3 +257,18 @@ VALUES
     'White',
     5
   );
+
+
+
+
+/* added inventory update querry */
+
+
+  UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
